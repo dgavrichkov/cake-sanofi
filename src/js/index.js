848adf49f1999577c23a0df002075e63ed0fbb4d
dgavrichkov <dgavrichkov32@gmail.com>
2021-07-29 16:10:53 +0300

    табы в попапе

--- src/js/index.js
+++ src/js/index.js
@@ -18,7 +18,39 @@ const episodes = function() {
     })
 };
 
+const tablist = function() {
+    const el = document.querySelector(".popup-tab");
+    if(!el) {
+        return false;
+    }
+    const tabControls = el.querySelectorAll(".popup-tab__control");
+    const tabPanels = el.querySelectorAll(".popup-tab__tab");
+    const tabCaret = el.querySelector(".popup-tab__indicator-caret");
+
+    let activeTab = Array.from(tabControls).find(tab => tab.classList.contains("is-active"));
+    let activePanel = Array.from(tabPanels).find(panel => panel.classList.contains("is-active"));
+
+    setCaretWidth();
+
+    tabControls.forEach(tab => {
+        tab.addEventListener("click", () => {
+            activeTab.classList.remove("is-active");
+            activePanel.classList.remove("is-active");
+            activeTab = tab;
+            activePanel = Array.from(tabPanels).find(panel => panel.dataset.tabPanel === tab.dataset.tabControl);
+            activeTab.classList.add("is-active");
+            activePanel.classList.add("is-active");
+            setCaretWidth();
+        });
+    });
+
+    function setCaretWidth() {
+        tabCaret.style.width = activeTab.offsetWidth + "px";
+        tabCaret.style.left = activeTab.offsetLeft + "px";    
+    }
+}
 
 window.addEventListener("DOMContentLoaded", function() {
     episodes();
+    tablist();
 });
